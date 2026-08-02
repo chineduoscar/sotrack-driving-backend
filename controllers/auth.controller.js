229bfc673a54923ebec6d654f788d4259da5c958
chineduoscar <chinedunnadoziepatrick@gmail.com>
2026-08-02 23:21:39 +0100

    fixing the signup controller

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -31,25 +31,6 @@ export const signup = async (req, res) => {
       password: hashedPassword,
     });
 
-    const token = jwt.sign(
-      {
-        id: user._id,
-        role: user.role,
-        fullName: user.fullName,
-        email: user.email,
-      },
-      process.env.JWT_SECRET,
-      { expiresIn: "7d" },
-    );
-
-    res.cookie("token", token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
-      maxAge: 7 * 24 * 60 * 60 * 1000,
-      path: "/",
-    });
-
     res.status(201).json({
       success: true,
       message: "Account created successfully.",
